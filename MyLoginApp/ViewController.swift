1a7d9265534db303232962dfcfceb007d89462c1
JohnKostanov <kostanovd@icloud.com>
2019-06-29 11:48:31 +0200

    Initial Commit

---
+++ MyLoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLoginApp
+//
+//  Created by  Джон Костанов on 29/06/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
